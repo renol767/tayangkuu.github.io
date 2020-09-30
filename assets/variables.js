02f5c644728a83510de1a54dac856254ba9ce121
renol767 <72129061+renol767@users.noreply.github.com>
2020-09-30 15:55:59 +0700

    Add files via upload

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Nick", "Cewe", "Akan", "Di", "Random"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
