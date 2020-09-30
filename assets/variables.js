23628ed1c394fdefc49899545ce858baf1d5a315
renol767 <72129061+renol767@users.noreply.github.com>
2020-09-30 16:15:21 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,5 @@
 export default {
-  nicknames: ["Nick", "Cewe", "Akan", "Di", "Random"],
+  nicknames: ["Nabilla", "De", "Tayang", "Sayang", "Dede emeushhh"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
